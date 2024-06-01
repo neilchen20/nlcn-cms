75d344f640f46c513ac2edc05ef0a6321269097e
Neil <chenweining960227@gmail.com>
2024-06-01 11:49:33 +0800

    strapi reinstall v3 to v4

---
+++ src/admin/app.js
@@ -0,0 +1,14 @@
+export default {
+  config: {
+    locales: ['zh'],
+    translations: {
+      'zh': {
+        'Auth.form.email.label': 'neil.cwn20@gmail.com',
+        Users: 'neil.cwn20@gmail.com',
+        City: 'Chinese(zh)',
+        Id: '1',
+      },
+    },
+  },
+  bootstrap() {},
+}
