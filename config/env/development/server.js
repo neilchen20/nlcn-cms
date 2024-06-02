3e6a99d8f8c43fb4b6332c2062bb508203dc63b6
Neil <chenweining960227@gmail.com>
2024-06-02 21:18:43 +0800

    rebuild

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   proxy: true,
   host: "0.0.0.0",
   port: process.env.PORT,
-  url: env('MY_HEROKU_URL','MY_ADMIN_URL','https://www.nlcn.me/project','https://www.nlcn.me/interest','https://nlcn.me/project','https://nlcn.me/interest'),
+  url: env('MY_HEROKU_URL'),
   app: { 
     keys: env.array('APP_KEYS')
   },
