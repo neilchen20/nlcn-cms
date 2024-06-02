c748b4cb93606db79385dcca2c9ef134d1124235
Neil <chenweining960227@gmail.com>
2024-06-02 10:46:54 +0800

    add cors origin for nlcn.me

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,22 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: false,
+    },
+  },
   'strapi::security',
   'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: process.env.CORS_ORIGIN.split(','),
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,7 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  settings: {
+    "users-permissions": {
+      jwtSecret: env("JWT_SECRET", "nlcn-20"),
+    },
+  },
+});
