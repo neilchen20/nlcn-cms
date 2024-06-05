fe0d6577c030305e75bc306aef9f010df9920df8
Neil Chen <chenweining960227@gmail.com>
2024-06-05 16:20:07 +0800

    Update database.js change db

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -9,11 +9,11 @@ module.exports = ({ env }) => {
     connection: {
       client: 'postgres',
       connection: {
-        host: host || env('DATABASE_HOST', 'ccba8a0vn4fb2p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com'),
-        port: port || env.int('DATABASE_PORT', 5432),
-        database: database || env('DATABASE_NAME', 'd5bu9g4j39ngdb'),
-        user: user || env('DATABASE_USERNAME', 'u1l7as8nc6sejs'),
-        password: password || env('DATABASE_PASSWORD', 'p5de5d8b5c827ce34897c6db204c168f87e6a53e04152e69c73477d7de6c9b867'),
+        host: host || env('DATABASE_HOST', 'hnd1.clusters.zeabur.com'),
+        port: port || env.int('DATABASE_PORT', 30199),
+        database: database || env('DATABASE_NAME', 'zeabur'),
+        user: user || env('DATABASE_USERNAME', 'root'),
+        password: password || env('DATABASE_PASSWORD', '0JTXnv45tZV3wMWgAC9j1YNK8Fqe267o'),
         ssl: { rejectUnauthorized: false },
       },
       debug: false,
