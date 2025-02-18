37d372179228ccc694cf4681669d31a0cfb33d1b
Neil <chenweining960227@gmail.com>
2025-02-18 17:01:34 +0800

    添加持久性儲存資料夾設定

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,19 +10,19 @@ module.exports = [
           "connect-src": ["'self'", "https:", "ws:", "wss:", "http:"],
           "img-src": ["'self'", "data:", "blob:",],
           "media-src": ["'self'", "data:", "blob:"],
-          'frame-src':["'self'","https://neilc.me/"],
+          'frame-src': ["'self'", "https://neilc.me/"],
           'frame-ancestors': null,
           upgradeInsecureRequests: null,
         },
       },
-      frameguard:  false,
+      frameguard: false,
     },
   },
   'strapi::cors',
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://neilc.me','https://www.neilc.me','https://admin.neilc.me','http://localhost:1337','http://localhost:5173'],
+      origin: ['https://neilc.me', 'https://www.neilc.me', 'https://admin.neilc.me', 'http://localhost:1337', 'http://localhost:5173'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
     },
@@ -32,6 +32,21 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public',
+  {
+    name: 'strapi::public',
+    config: {
+      path: '/data/public',  // 確保公開資源存儲到 Zeabur 的 Volume
+      maxAge: 31536000,
+    },
+  },
+  {
+    name: 'strapi::upload',
+    config: {
+      providerOptions: {
+        localServer: {
+          folderPath: '/data/uploads',  // 設定上傳媒體的目錄
+        },
+      },
+    },
+  },
 ];
-// sss
